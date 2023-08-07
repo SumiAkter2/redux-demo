5766e075e15d63d45e8433d318379408a62e7da0
SumiAkter2 <akteralisha36@gmail.com>
2023-08-07 14:22:51 +0600

    added reducer

--- index.js
+++ index.js
@@ -8,3 +8,21 @@ function buyCake() {
     info: "first redux action",
   };
 }
+
+// reducer:
+// (previousState,action)=>newState;
+const initialState = {
+  numOfCake: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCake: state.numOfCake - 1,
+      };
+    default:
+      return state;
+  }
+};
