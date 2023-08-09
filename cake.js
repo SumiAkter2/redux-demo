c12db1ae6b9498f266fe548a8a4f4bb827ebdf9e
SumiAkter2 <akteralisha36@gmail.com>
2023-08-09 20:46:48 +0600

    added reducer

--- cake.js
+++ cake.js
@@ -1,1 +1,24 @@
-console.log("cake shop");
+const BUY_CAKE = "BUY_CAKE";
+
+const buyCake = () => {
+  return {
+    type: BUY_CAKE,
+    quantity: 1,
+  };
+};
+
+const initialState = {
+  numOfCake: 10,
+};
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCake: state.numOfCake - 1,
+      };
+
+    default:
+      return state;
+  }
+};
